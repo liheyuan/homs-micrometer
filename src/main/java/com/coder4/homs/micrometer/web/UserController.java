6b5154cff849d7a865191eb463e78a656386ad88
coder4 <liheyuan.bj@gmail.com>
2022-06-27 22:22:58 +0800

    MOD: Basic MeterRegistry Usage with JMX

---
+++ src/main/java/com/coder4/homs/micrometer/web/UserController.java
@@ -0,0 +1,37 @@
+package com.coder4.homs.micrometer.web;
+
+
+import com.coder4.homs.micrometer.web.data.UserVO;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private MeterRegistry meterRegistry;
+
+    private Counter COUNTER_GET_USER;
+
+    @PostConstruct
+    public void init() {
+        COUNTER_GET_USER = meterRegistry.counter("app_requests_method_count", "method", "UserController.getUser");
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public UserVO getUser(@PathVariable int id) {
+        UserVO user = new UserVO();
+        user.setId(id);
+        user.setName(String.format("user_%d", id));
+
+        COUNTER_GET_USER.increment();
+        return user;
+    }
+
+}
